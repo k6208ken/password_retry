0c57422d375c9b672f928a13c3f80a031fd326c4
k6208ken <a618613915@gmail.com>
2018-08-25 14:00:22 +0800

    final_answer

--- password_retry.py
+++ password_retry.py
@@ -1,11 +1,14 @@
 password = 'a123456'
 i = 3 #剩餘機會
 while i > 0:
+	i -= 1
 	pwd = input('請輸入密碼：')
 	if pwd == password:
 		print('登入成功！')
 		break
 	else:
-		i -= 1
-		print('密碼錯誤 還有{}次機會'.format(i))
+		print('密碼錯誤!')
+		if i > 0:
+			print('還有{}次機會'.format(i))
+
 		
